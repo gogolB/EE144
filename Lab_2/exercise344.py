e0ae71ba687d47174da35f741edc07103b1d5f39
Gogol Bhattacharya <sbhat006@ucr.edu>
2018-04-17 13:57:41 -0700

    Finished 3.44, need to add in more testing

--- Lab_2/exercise344.py
+++ Lab_2/exercise344.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 
-import modern_robotics
+from  modern_robotics import *
+import numpy as np
 
 ''' 	Exercise 3.44 from modern robots book
 	Write a function that returns "True" if a given 4x4 Matrix is within
 	episilon of an element of SE(3) and returns "false" otherwise.
 '''
 
-def function_exercise344():
-	pass;
+def function_exercise344(matrixT, episilon):
+	
+	R = matrixT[np.ix_([0,1,2], [0,1,2])]
 
+	return np.all(abs(R.T*R - np.identity(3)) < episilon) 
+	
+	#return False;
+
+
+def testharness():
+	matrixT = np.identity(4);
+	print(function_exercise344(matrixT, 1e-7))
