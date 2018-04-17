0b749f8d39f81be59fe6c0dd6aa32d97c8341e50
Gogol Bhattacharya <sbhat006@ucr.edu>
2018-04-17 14:21:27 -0700

    Working on exercise 3.46

---
+++ Lab_2/exercise346.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import numpy as np
+
+'''	Exercise 3.46
+	Write a function that returns "true" if given a 4x4 matrix is 
+	within episilon of of an element of se(3) and false otherwise
+'''
+
+def function_exercise346(matrixT, episilon):
+
+	wb = matrixT[np.ix_([0,1,2],[0,1,2])];
+
+	return false;
