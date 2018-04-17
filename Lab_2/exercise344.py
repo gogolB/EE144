1951ec570555b98813962bc1ee50d7e78039169f
Gogol Bhattacharya <sbhat006@ucr.edu>
2018-04-17 13:31:18 -0700

    Added in modern_robotics.py and exercise344.py'

---
+++ Lab_2/exercise344.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import modern_robotics
+
+''' 	Exercise 3.44 from modern robots book
+	Write a function that returns "True" if a given 4x4 Matrix is within
+	episilon of an element of SE(3) and returns "false" otherwise.
+'''
+
+def function_exercise344():
+	pass;
+
