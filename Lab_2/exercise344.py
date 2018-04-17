a4ec2a6fb74414700dbf9ac28292ea8b63ce00e4
Gogol Bhattacharya <sbhat006@ucr.edu>
2018-04-17 14:07:07 -0700

    Added in another test case

--- Lab_2/exercise344.py
+++ Lab_2/exercise344.py
@@ -12,10 +12,17 @@ def function_exercise344(matrixT, episilon):
 	
 	R = matrixT[np.ix_([0,1,2], [0,1,2])]
 
-	return np.all(abs(R.T*R - np.identity(3)) < episilon) and (np.linalg.det(R) - 1 < episilon)
+	return (matrixT.shape == (4,4)) and (np.all(abs(R.T*R - np.identity(3)) < episilon)) and (np.linalg.det(R) - 1 < episilon)
 	#return False;
 
 
 def testharness():
+	print('Testing a 3x3 identity matrix...')
+	matrixT = np.identity(3);
+	print(function_exercise344(matrixT,1e-7))	
+
+	print('Testing a 4x4 identity matrix')
 	matrixT = np.identity(4);
 	print(function_exercise344(matrixT, 1e-7))
+
+	# Other Test cases.
