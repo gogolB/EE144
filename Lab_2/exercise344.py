d4fec57d8e83e4c62fe1a8041d87c72a0a2a9ea9
Gogol Bhattacharya <sbhat006@ucr.edu>
2018-04-17 14:48:50 -0700

    Edited 3.44 to check bottom row

--- Lab_2/exercise344.py
+++ Lab_2/exercise344.py
@@ -12,7 +12,16 @@ def function_exercise344(matrixT, episilon):
 	
 	R = matrixT[np.ix_([0,1,2], [0,1,2])]
 
-	return (matrixT.shape == (4,4)) and (np.all(abs(R.T*R - np.identity(3)) < episilon)) and (np.linalg.det(R) - 1 < episilon)
+	is_SO3 = np.all(abs(R.T*R - np.identity(3)) < episilon)
+	is_4x4 = matrixT.shape == (4,4)
+	is_det1 = np.linalg.det(R) - 1 < episilon
+
+	if(is_4x4):
+		bottomRowCorrect = matrixT[3,0] == 0 and matrixT[3,1] == 0 and matrixT[3,2] == 0 and matrixT[3,3] == 1
+	else:
+		bottomRowCorrect = False;
+
+	return bottomRowCorrect and is_SO3 and is_4x4 and is_det1
 	#return False;
 
 
@@ -26,3 +35,6 @@ def testharness():
 	print(function_exercise344(matrixT, 1e-7))
 
 	# Other Test cases.
+	print('Testing a 4x4 identity with (3,3) = 0')
+	matrixT[3,3] = 0;
+	print(function_exercise344(matrixT, 1e-7))
