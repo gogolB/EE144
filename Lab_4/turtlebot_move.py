6b3e6ff6d7a6a458ca0eb0e130d6800a5500164b
Gogol Bhattacharya <acx234@gmail.com>
2018-04-24 13:42:39 -0700

    Made turtle bot cleaner

--- Lab_4/turtlebot_move.py
+++ Lab_4/turtlebot_move.py
@@ -1,58 +1,80 @@
 #!/usr/bin/env python
 
 import rospy
+from std_msgs.msg import String, Empty
+from time import time
+import tf
 from geometry_msgs.msg import Twist
 
 class turtlebot_move():
-    def __init__(self):
-        rospy.init_node('turtlebot_move', anonymous=False)
+	def __init__(self):
+		rospy.init_node('turtlebot_move', anonymous=False)
 
-	rospy.loginfo("Press CTRL + C to terminate")
- 
-        rospy.on_shutdown(self.shutdown)
+		rospy.loginfo("Press CTRL + C to terminate")
+		rospy.on_shutdown(self.shutdown)
+		
+		self.reset_odom = rospy.Publisher('mobile_base/commands/reset_odometry', Empty, queue_size = 10)
+		self.set_velocity = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+
+		timer=time()
+		while time()-timer<1.0:
+			self.reset_odom.publish(Empty())
+	
+		self.tfListener = tf.TransformListener();
+	
+		# Move forward at 0.5 m/s
+		self.vel = Twist()
+		self.vel.linear.x = 0.5
+		self.vel.angular.z = 0
+
+		# Rotate in Place at 90 Degrees Per second.
+		self.rotate_vel = Twist()
+		self.rotate_vel.linear.x = 0
+		self.rotate_vel.angular.z = 1.57075
+
+		# Set update rate at 10 Hz
+		self.rate = rospy.Rate(10);                        
         
-        self.set_velocity = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+	def shutdown(self):
+		rospy.loginfo("Stop Action")
+		stop_vel = Twist()
+		stop_vel.linear.x = 0
+		stop_vel.angular.z = 0
+		self.set_velocity.publish(stop_vel)
+		rospy.sleep(1)
 
-	# Move forward at 0.5 m/s
-        vel = Twist()
-        vel.linear.x = 0.5
-	vel.angular.z = 0
+	def setVel(self, twist):
+		self.set_velocity.publish();
 
-	# Rotate in Place at 90 Degrees Per second.
-	rotate_vel = Twist()
-        rotate_vel.linear.x = 0
-	rotate_vel.angular.z = 1.57075
+	def sleep(self):
+		self.rate.sleep()
 
-	# Set update rate at 10 Hz
-	rate = rospy.Rate(10);
+	def makeSquare(self):
+		while True:
+			try:
+				(position, quaternion) = self.tfListener.lookupTransform("/odom", "/base_footprint", rospy.Time(0))
+				orientation = tf.transformations.euler_from_quaternion(quaternion)
+				rospy.loginfo("position: "+str(position))
+				rospy.loginfo("orientation: "+str(orientation))
+			except:
+				continue;
 
-	# Run this loop forever.
-	while True:
-		# Move forward for 50 time ticks. (0.5 m/s * 10 seconds * 10 ticks per second)
-		for i in range(50):
-			self.set_velocity.publish(vel)
-            		rate.sleep()
-		rospy.loginfo("Moved 5 M...")
-		# Rotate in place for 10 time ticks.
-		for i in range(10):
-			self.set_velocity.publish(rotate_vel)
-    			rate.sleep()
-		rospy.loginfo("Turned 90 Deg...")
-                        
-        
-    def shutdown(self):
+			# Move forward for 50 time ticks. (0.5 m/s * 10 seconds * 10 ticks per second)
+			for i in range(50):
+				self.setVel(self.vel)
+				self.sleep()
+			rospy.loginfo("Moved 5 M...")
 
-        rospy.loginfo("Stop Action")
-	stop_vel = Twist()
-        stop_vel.linear.x = 0
-	stop_vel.angular.z = 0
-        self.set_velocity.publish(stop_vel)
-
-        rospy.sleep(1)
- 
+			# Rotate in place for 10 time ticks.
+			for i in range(10):
+				self.setVel(self.rotate_vel)
+				self.sleep()
+			rospy.loginfo("Turned 90 Deg...")	
+	
 if __name__ == '__main__':
-    try:
-        turtlebot_move()
-    except rospy.ROSInterruptException:
-        rospy.loginfo("Action terminated.")
+	try:
+		tb = turtlebot_move()
+		tb.makeSquare()
+	except rospy.ROSInterruptException:
+		rospy.loginfo("Action terminated.")
 
