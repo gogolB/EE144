6636e8480b9ec16fdf484717f9b269ca4f8a7c16
Gogol Bhattacharya <acx234@gmail.com>
2018-04-24 16:05:46 -0700

    Worked on lab 4

--- Lab_4/turtlebot_move.py
+++ Lab_4/turtlebot_move.py
@@ -5,6 +5,7 @@ from std_msgs.msg import String, Empty
 from time import time
 import tf
 from geometry_msgs.msg import Twist
+import numpy as np;
 
 class turtlebot_move():
 	def __init__(self):
@@ -33,7 +34,11 @@ class turtlebot_move():
 		self.rotate_vel.angular.z = 1.57075
 
 		# Set update rate at 10 Hz
-		self.rate = rospy.Rate(10);                        
+		self.rate = rospy.Rate(100);
+
+		self.sideLength = 5;
+		self.pos = None;
+		self.rot = None;                     
         
 	def shutdown(self):
 		rospy.loginfo("Stop Action")
@@ -44,7 +49,7 @@ class turtlebot_move():
 		rospy.sleep(1)
 
 	def setVel(self, twist):
-		self.set_velocity.publish();
+		self.set_velocity.publish(twist);
 
 	def sleep(self):
 		self.rate.sleep()
@@ -60,21 +65,224 @@ class turtlebot_move():
 				continue;
 
 			# Move forward for 50 time ticks. (0.5 m/s * 10 seconds * 10 ticks per second)
-			for i in range(50):
+			for i in range(500):
 				self.setVel(self.vel)
 				self.sleep()
 			rospy.loginfo("Moved 5 M...")
 
 			# Rotate in place for 10 time ticks.
-			for i in range(10):
+			for i in range(100):
 				self.setVel(self.rotate_vel)
 				self.sleep()
-			rospy.loginfo("Turned 90 Deg...")	
-	
+			rospy.loginfo("Turned 90 Deg...")
+
+	def getPos(self):
+		try:
+			(self.pos, quaternion) = self.tfListener.lookupTransform("/odom", "/base_footprint", rospy.Time(0))
+			self.rot = tf.transformations.euler_from_quaternion(quaternion)
+			if(self.rot[2] < 0):
+				newZ = 3.1415 + (3.1415 - abs(self.rot[2]))
+				self.rot = (self.rot[0],self.rot[1],newZ)
+		except:
+			return
+
+	def makeSquareP(self):
+		# First get orientation
+		self.getPos();
+		while(self.pos is None or self.rot is None):
+			self.getPos();
+		
+		print("Pos: " + str(self.pos))
+		print("Orentation: " + str(self.rot));
+		
+		desiredOrientation = self.rot[2];
+		desiredPosition = np.array(self.pos, copy=True);
+		desiredPosition[0] = desiredPosition[0] + self.sideLength;
+		print("Desired Position: " + str(desiredPosition))
+		
+		kp = 2;
+
+		# Move to new location
+		while(abs(desiredPosition[0] - self.pos[0]) > 1e-2):
+			#print("Distance to Go: " + str(abs(desiredPosition[0] - self.pos[0])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0.5
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();
+
+		stop_vel = Twist()
+		stop_vel.linear.x = 0
+		stop_vel.angular.z = 0
+		self.set_velocity.publish(stop_vel)	
+
+		print("Error: " + str(abs(desiredPosition[0] - self.pos[0])))
+		
+		desiredOrientation = self.rot[2] + 3.151529/2;
+		print("Desired Orientation: " + str(desiredOrientation))
+
+		# Turn 90 degrees
+		while(abs(desiredOrientation - self.rot[2]) > 1e-2):
+			#print("Rotation to Go: " + str(abs(desiredOrientation - self.rot[2])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();	
+
+		self.getPos();		
+		print("Pos: " + str(self.pos))
+		print("Orentation: " + str(self.rot));
+		
+		print("Error: " + str(abs(desiredOrientation - self.rot[2])))
+		
+		# Move to Location 2
+		desiredOrientation = self.rot[2];
+		desiredPosition = np.array(self.pos, copy=True);
+		desiredPosition[1] = desiredPosition[1] + self.sideLength;
+		print("Desired Position 2: " + str(desiredPosition))
+
+		while(abs(desiredPosition[1] - self.pos[1]) > 1e-2):
+			#print("Distance to Go: " + str(abs(desiredPosition[1] - self.pos[1])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0.5
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();
+
+		stop_vel = Twist()
+		stop_vel.linear.x = 0
+		stop_vel.angular.z = 0
+		self.set_velocity.publish(stop_vel)	
+
+		print("Error: " + str(abs(desiredPosition[1] - self.pos[1])))
+
+		desiredOrientation = self.rot[2] + 3.151529/2;
+		print("Desired Orientation: " + str(desiredOrientation))
+
+		# Turn 90 degrees
+		while(abs(desiredOrientation - self.rot[2]) > 1e-1):
+			print("Rotation to Go: " + str(abs(desiredOrientation - self.rot[2])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();	
+
+		self.getPos();		
+		print("Pos: " + str(self.pos))
+		print("Orentation: " + str(self.rot));
+		
+		print("Error: " + str(abs(desiredOrientation - self.rot[2])))
+		
+
+		# Move to Location 3		
+		desiredOrientation = self.rot[2];
+		desiredPosition = np.array(self.pos, copy=True);
+		desiredPosition[0] = desiredPosition[0] - self.sideLength;
+		print("Desired Position 3: " + str(desiredPosition))
+		while(abs(desiredPosition[0] - self.pos[0]) > 1e-2):
+			print("Rotation adjust: " + str(desiredOrientation - self.rot[2]))
+			w = kp * (desiredOrientation - self.rot[2]);
+
+			vel = Twist()
+			vel.linear.x = 0.5
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();
+
+		stop_vel = Twist()
+		stop_vel.linear.x = 0
+		stop_vel.angular.z = 0
+		self.set_velocity.publish(stop_vel)	
+
+		print("Error: " + str(abs(desiredPosition[0] - self.pos[0])))
+		
+		desiredOrientation = self.rot[2] + 3.151529/2;
+		print("Desired Orientation: " + str(desiredOrientation))
+
+		# Turn 90 degrees
+		while(abs(desiredOrientation - self.rot[2]) > 1e-1):
+			print("Rotation to Go: " + str(abs(desiredOrientation - self.rot[2])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();	
+
+		self.getPos();		
+		print("Pos: " + str(self.pos))
+		print("Orentation: " + str(self.rot));
+		
+		print("Error: " + str(abs(desiredOrientation - self.rot[2])))
+		
+		# Move to Location 4
+		desiredOrientation = self.rot[2];
+		desiredPosition = np.array(self.pos, copy=True);
+		desiredPosition[1] = desiredPosition[1] - self.sideLength;
+		print("Desired Position 2: " + str(desiredPosition))
+
+		while(abs(desiredPosition[1] - self.pos[1]) > 1e-2):
+			print("Distance to Go: " + str(abs(desiredPosition[1] - self.pos[1])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0.5
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();
+
+		stop_vel = Twist()
+		stop_vel.linear.x = 0
+		stop_vel.angular.z = 0
+		self.set_velocity.publish(stop_vel)	
+
+		print("Error: " + str(abs(desiredPosition[1] - self.pos[1])))
+
+		desiredOrientation = self.rot[2] + 3.151529/2;
+		print("Desired Orientation: " + str(desiredOrientation))
+
+		# Turn 90 degrees
+		while(abs(desiredOrientation - self.rot[2]) > 1e-2):
+			print("Rotation to Go: " + str(abs(desiredOrientation - self.rot[2])))
+			w = kp * (desiredOrientation - self.rot[2]);
+			vel = Twist()
+			vel.linear.x = 0
+			vel.angular.z = w
+			
+			self.setVel(vel);			
+			self.sleep();
+			self.getPos();	
+
+		self.getPos();		
+		print("Pos: " + str(self.pos))
+		print("Orentation: " + str(self.rot));
+		
+		print("Error: " + str(abs(desiredOrientation - self.rot[2])))	
+			
+			
 if __name__ == '__main__':
 	try:
 		tb = turtlebot_move()
-		tb.makeSquare()
+		tb.makeSquareP()
 	except rospy.ROSInterruptException:
 		rospy.loginfo("Action terminated.")
 
